8db9553e68644b46934690958862b9c839dd64d4
Eyad <eyadscript@gmail.com>
2025-09-22 12:17:33 +0300

    Update Do1ngData.js

--- script/Do1ngData.js
+++ script/Do1ngData.js
@@ -1,5 +1,12 @@
 // Data array to hold the content details
-        const contentData = [
+const contentData = [
+            {
+                url: 'https://do1ng.com/products/web-scraper-script',
+                imgSrc: 'https://do1ng.com/_next/image?url=https%3A%2F%2Fapi.do1ng.com%2Fstorage%2F928%2Flocalhost_3000_.png&w=1920&q=75',
+                description: 'Key Features: Precise Data Extraction Extracts text, HTML structure, attributes, and classes from any page element using CSS selectors. Supports waiting for dynamic content with the Wait For Selector option. User-Friendly Interface A modern design with an attractive color scheme and intuitive layout. Displays results in three formats: Table, JSON, and Raw Text. Advanced Options Customizable timeout settings to prevent errors on slow-loading pages. Option to include HTML code of extracted elements. Data Export Export results to JSON and CSV formats with a single click. Element Details Viewer A detailed modal displays all attributes and HTML code of the selected element. High Performance Runs in the background using Puppeteer for efficient data extraction, even from complex pages. Supports security configurations like `--no-sandbox` for compatibility across different servers. How It Works? Enter the website URL and the CSS selector for the data you want to extract. Adjust advanced settings if needed. Click Scrape Now to begin extraction. View results or download them in your preferred format. Technologies Used: Backend: Node.js, Express.js, Puppeteer Frontend: HTML5, CSS3, Vanilla JavaScript Formats: JSON, CSV',
+                price: '$7.00',
+                iconSrc: 'image/apple-icon-180.webp'
+            },
             {
                 url: 'https://do1ng.com/products/sign-in-&-sign-up-(1)',
                 imgSrc: 'https://do1ng.com/_next/image?url=https%3A%2F%2Fapi.do1ng.com%2Fstorage%2F1136%2Fsnap2-(1).png&w=1920&q=75',
