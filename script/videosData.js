9fb3a00f08268e7b57ce5318eb850d794e458c24
Eyad <eyadscript@gmail.com>
2025-09-22 12:13:29 +0300

    Update videosData.js

--- script/videosData.js
+++ script/videosData.js
@@ -2,5 +2,11 @@ const videosData = {
     Ey001: "https://www.youtube.com/embed/Fzv-rgwcFKk",
     Ey002: "https://www.youtube.com/embed/P8YuWEkTeuE",
     Ey003: "https://www.youtube.com/embed/NljIHlZRTTE",
-    Ey004: "https://www.youtube.com/embed/_sxoqRIbW0c"
+    Ey004: "https://www.youtube.com/embed/_sxoqRIbW0c",
+    Ey005: "https://www.youtube.com/embed/FYFmQEpZywc",
+    Ey006: "https://www.youtube.com/embed/GXlckaGr0Eo",
+    Ey007: "https://www.youtube.com/embed/VAjbRrZm80s",
+    Ey008: "https://www.youtube.com/embed/RUG8yFJKVco",
+    Ey009: "https://www.youtube.com/embed/37KohMnlP7Q",
+    Ey0010: "https://www.youtube.com/embed/MJUssi2c6Ls"
 };
