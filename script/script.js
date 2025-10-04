aca405792a78c9036249ecd9894edfe43973663e
Eyad <eyadscript@gmail.com>
2025-10-04 10:35:39 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -399,6 +399,7 @@ document.addEventListener("DOMContentLoaded", () => {
                         </div>
                     </button>
                     <div class="mbvideo-im Wave-cloud">
+                        <vido class="flex"><svg xmlns="http://www.w3.org/2000/svg" class="ionicon" style="height: 25px;left: 1px; position: relative;" viewBox="0 0 512 512"><path d="M133 440a35.37 35.37 0 01-17.5-4.67c-12-6.8-19.46-20-19.46-34.33V111c0-14.37 7.46-27.53 19.46-34.33a35.13 35.13 0 0135.77.45l247.85 148.36a36 36 0 010 61l-247.89 148.4A35.5 35.5 0 01133 440z"/></svg></vido>
                         <img alt="Video Thumbnail" src="https://img.youtube.com/vi/${videoId}/hqdefault.jpg" loading="lazy">
                     </div>
                     <div class="description">
