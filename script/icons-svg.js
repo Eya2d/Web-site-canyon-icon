f34504be6b1faf41a0b55e63a91971c8a4af37d9
Eyad <eyadscript@gmail.com>
2025-10-06 14:57:39 +0300

    Update icons-svg.js

--- script/icons-svg.js
+++ script/icons-svg.js
@@ -98,6 +98,9 @@ const iconMap = {
   code: `
 <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><rect x="128" y="128" width="336" height="336" rx="57" ry="57" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"/><path d="M383.5 128l.5-24a56.16 56.16 0 00-56-56H112a64.19 64.19 0 00-64 64v216a56.16 56.16 0 0056 56h24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/></svg>
 `,
+  filter: `
+<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M32 144h448M112 256h288M208 368h96"/></svg>
+`,
 };
 
 // تحويل جميع عناصر <icon> إلى SVG
