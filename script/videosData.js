1671f7ab3555ccb9f1ddcf0ebd57aa864355b8b9
Eyad <eyadscript@gmail.com>
2025-10-12 13:10:20 +0300

    Update videosData.js

--- script/videosData.js
+++ script/videosData.js
@@ -1,12 +1,14 @@
 const videosData = {
-    Ey001: "https://www.youtube.com/embed/Fzv-rgwcFKk",
-    Ey002: "https://www.youtube.com/embed/P8YuWEkTeuE",
-    Ey003: "https://www.youtube.com/embed/NljIHlZRTTE",
-    Ey004: "https://www.youtube.com/embed/_sxoqRIbW0c",
-    Ey005: "https://www.youtube.com/embed/FYFmQEpZywc",
-    Ey006: "https://www.youtube.com/embed/GXlckaGr0Eo",
-    Ey007: "https://www.youtube.com/embed/VAjbRrZm80s",
-    Ey008: "https://www.youtube.com/embed/RUG8yFJKVco",
-    Ey009: "https://www.youtube.com/embed/37KohMnlP7Q",
-    Ey010: "https://www.youtube.com/embed/MJUssi2c6Ls"
+  Ey001: "https://www.youtube.com/embed/Fzv-rgwcFKk",
+  Ey002: "https://www.youtube.com/embed/P8YuWEkTeuE",
+  Ey003: "https://www.youtube.com/embed/NljIHlZRTTE",
+  Ey004: "https://www.youtube.com/embed/_sxoqRIbW0c",
+  Ey005: "https://www.youtube.com/embed/FYFmQEpZywc",
+  Ey006: "https://www.youtube.com/embed/GXlckaGr0Eo",
+  Ey007: "https://www.youtube.com/embed/VAjbRrZm80s",
+  Ey008: "https://www.youtube.com/embed/RUG8yFJKVco",
+  Ey009: "https://www.youtube.com/embed/37KohMnlP7Q",
+  Ey010: "https://www.youtube.com/embed/MJUssi2c6Ls",
+  Ey011: "https://www.youtube.com/embed/Op1SgQ-RlFA",
+  Ey012: "https://www.youtube.com/embed/C0lxebxgrrI"
 };
